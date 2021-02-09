8b9b617ab76114be0cad909bee4a2519969f9698
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-02-09 16:40:57 +0100

    update javascript

--- pokedex.js
+++ pokedex.js
@@ -1,72 +1,91 @@
+
 document.getElementById("run").addEventListener("click", () => {
     console.log("click")
-    document.getElementById("target").innerText = "";
+
+    //gets value from input field (users enter pokemon ID or name)
     let searchValue = document.getElementById("poke-id").value;
 
-    const pokeTemplate = document.getElementById("tpl-poke").content;
+//here we start to work with data that we fetched by async function pokemonData
 
+    //uses the pokemon data of the specific pokemon from the input for getting picture,name,moves,ID
     pokemonData(searchValue).then(pokemon => {
-            console.log(pokemon)
 
-            let imageURL = pokemon.sprites.front_default;
-            let name = pokemon.name;
-            let fourMoves = [];
-                let moves = pokemon.moves.slice(0,4);
-                    moves.forEach(move => fourMoves.push(move.move.name));
+        //gets picture of the specific version of pokemon
+        let imageURL = pokemon.sprites.front_default;
+        //gets name
+        let name = pokemon.name;
+        //array to store only 4 moves (I choose simply just first 4)
+        let fourMoves = [];
+        //gets the first 4 moves from pokemon.moves array (API)
+        let moves = pokemon.moves.slice(0, 4);
+        //put each move one by one into new created array above "let fourMoves"
+        moves.forEach(move => fourMoves.push(move.move.name));
+        //gets ID
+        let pokemonID = pokemon.id;
+        //gets the URL of the previous evolution pokemon
+        let speciesUrl = pokemon.species.url;
 
 
-            let pokemonID = pokemon.id;
-            let speciesUrl = pokemon.species.url;
-
-            console.log()
-            data(speciesUrl).then(evolutionChain => {
+        //gets the data from the speciesURL
+        data(speciesUrl).then(evolutionChain => {
                 let prevEvolution;
 
                 try {
-                    prevEvolution = evolutionChain.evolves_from_species.name;
+                    prevEvolution = evolutionChain.evolves_from_species.name;    //gets name from evolves_from_species => save the value in variable "prevEvolution"
                 } catch {
-                    prevEvolution = "";
+                    prevEvolution = "";                                          //if no previous evolution - no name appears
                 }
 
-                let pokeHTML = pokeTemplate.cloneNode(true);
 
-                pokeHTML.getElementById("show-id").innerText = pokemonID;
-                pokeHTML.getElementById("actualEvoImg").setAttribute("src",imageURL) ;
-                pokeHTML.querySelector(".name").innerText = name;
-                pokeHTML.querySelector(".evolutions").innerText = prevEvolution;
-                pokeHTML.getElementById("previousEvoImg").setAttribute("src",pokemon.species);
-                pokeHTML.querySelectorAll(".move").forEach((p ,index) => {
-                    p.innerText= fourMoves[index];
-                    if(fourMoves[index] === undefined){
-                        p.innerText = " - ";
+                //gets picture of the previous evolution pokemon, that is related to the one which was originally researched via user input
+                pokemonData(prevEvolution).then(prevEvolutionPoke => {
+                    let prevEvoImgURL
+
+                    try {
+                        prevEvoImgURL = prevEvolutionPoke.sprites.front_default; //gets pic (just front_default) from sprites => save the pic in variable "prevEvoImgURL"
+                    } catch {
+                        prevEvoImgURL = "";                                      //if no previous evolution - no picture appears
                     }
-                });
 
+                    //display data - which I get in the previous steps - in the HTML -> in the Pokedex
+                    //show ID of pokemon from input
+                    document.getElementById("show-id").innerText = pokemonID;
+                    //show pic of pokemon from input
+                    document.getElementById("actualEvoImg").setAttribute("src", imageURL);
+                    //show name of pokemon from input, with capital first letter
+                    document.querySelector(".name").innerText = name.charAt(0).toUpperCase() + name.slice(1);
+                    //show name of the previous evolution  of the pokemon from input, with capital first letter
+                    document.querySelector(".evolutions").innerText = prevEvolution.charAt(0).toUpperCase() + prevEvolution.slice(1);
+                    //show picture of the previous evolution of the pokemon from input
+                    document.getElementById("previousEvoImg").setAttribute("src", prevEvoImgURL);
+                    //show 4 moves in the <p> tag section -> in the Pokedex blue boxes
+                    //each move is with capital first letter
+                    document.querySelectorAll(".move").forEach((p, index) => {
+                        p.innerText = fourMoves[index].charAt(0).toUpperCase() + fourMoves[index].slice(1);
+                        //condition for case if pokemon has less than 4 moves (ditto)
+                        if (fourMoves[index] === undefined) {
+                            p.innerText = " - ";
+                        }
+                    });
 
+                })
+            }
+            , rej => console.error(rej))
+    });
 
-                document.getElementById("target").appendChild(pokeHTML);
-            })
-        }
-        , rej => console.error(rej))
-});
+    //gets the data from the specified URL (speciesUrl)
+    async function data(url) {
+        const response = await fetch(url);
+        return await response.json();
+    }
+    //gets data about pokemon
+    async function pokemonData(searchValue) {
 
-async function evolutionData(pokemonID) {
-    const response = await fetch("https://pokeapi.co/api/v2/evolution-chain/" + pokemonID);
-    return await response.json();
-}
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + searchValue);
+        console.log(response)
+        return await response.json();
+    }
 
-async function data(url) {
-    const response = await fetch(url);
-    return await response.json();
-}
-
-async function pokemonData(searchValue) {
-
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + searchValue);
-    console.log(response)
-    return await response.json();
-}
-
-
+})
 
 
