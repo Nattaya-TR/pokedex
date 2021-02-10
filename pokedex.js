01496407546229666abcb7d1f30589055caf29cd
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-02-10 09:02:01 +0100

    update Js and css

--- pokedex.js
+++ pokedex.js
@@ -1,9 +1,21 @@
-
 document.getElementById("run").addEventListener("click", () => {
-    console.log("click")
+    //condition if input field is empty
+    if (document.getElementById("poke-id").value === "") {
+        return;
+    }
+    onClick(document.getElementById("poke-id").value);
+})
+//on click show data for of previous evolutions
+document.getElementById("prevEvo").addEventListener("click", () => {
+    onClick(prevEvolution);
+});
+let prevEvolution;
 
+
+function onClick(pokemon) {
     //gets value from input field (users enter pokemon ID or name)
-    let searchValue = document.getElementById("poke-id").value;
+    let searchValue = pokemon;
+
 
 //here we start to work with data that we fetched by async function pokemonData
 
@@ -14,12 +26,10 @@ document.getElementById("run").addEventListener("click", () => {
         let imageURL = pokemon.sprites.front_default;
         //gets name
         let name = pokemon.name;
-        //array to store only 4 moves (I choose simply just first 4)
-        let fourMoves = [];
-        //gets the first 4 moves from pokemon.moves array (API)
-        let moves = pokemon.moves.slice(0, 4);
-        //put each move one by one into new created array above "let fourMoves"
-        moves.forEach(move => fourMoves.push(move.move.name));
+
+        //shuffles and gets first 4 random moves
+        let fourMoves = pokemon.moves.sort(() => 0.5 - Math.random()).slice(0, 4);
+
         //gets ID
         let pokemonID = pokemon.id;
         //gets the URL of the previous evolution pokemon
@@ -28,8 +38,6 @@ document.getElementById("run").addEventListener("click", () => {
 
         //gets the data from the speciesURL
         data(speciesUrl).then(evolutionChain => {
-                let prevEvolution;
-
                 try {
                     prevEvolution = evolutionChain.evolves_from_species.name;    //gets name from evolves_from_species => save the value in variable "prevEvolution"
                 } catch {
@@ -42,50 +50,68 @@ document.getElementById("run").addEventListener("click", () => {
                     let prevEvoImgURL
 
                     try {
-                        prevEvoImgURL = prevEvolutionPoke.sprites.front_default; //gets pic (just front_default) from sprites => save the pic in variable "prevEvoImgURL"
+                        prevEvoImgURL = prevEvolutionPoke.sprites.front_default;               //gets pic (just front_default) from sprites => save the pic in variable "prevEvoImgURL"
+                        document.getElementById("prevEvo").style.display = "block";
                     } catch {
-                        prevEvoImgURL = "";                                      //if no previous evolution - no picture appears
+                        prevEvoImgURL = "";                                                    //if no previous evolution - no picture appears
+                        document.getElementById("prevEvo").style.display = "none";
                     }
 
                     //display data - which I get in the previous steps - in the HTML -> in the Pokedex
                     //show ID of pokemon from input
                     document.getElementById("show-id").innerText = pokemonID;
+
                     //show pic of pokemon from input
                     document.getElementById("actualEvoImg").setAttribute("src", imageURL);
+
                     //show name of pokemon from input, with capital first letter
                     document.querySelector(".name").innerText = name.charAt(0).toUpperCase() + name.slice(1);
+
                     //show name of the previous evolution  of the pokemon from input, with capital first letter
                     document.querySelector(".evolutions").innerText = prevEvolution.charAt(0).toUpperCase() + prevEvolution.slice(1);
+
                     //show picture of the previous evolution of the pokemon from input
                     document.getElementById("previousEvoImg").setAttribute("src", prevEvoImgURL);
+
                     //show 4 moves in the <p> tag section -> in the Pokedex blue boxes
                     //each move is with capital first letter
                     document.querySelectorAll(".move").forEach((p, index) => {
-                        p.innerText = fourMoves[index].charAt(0).toUpperCase() + fourMoves[index].slice(1);
                         //condition for case if pokemon has less than 4 moves (ditto)
                         if (fourMoves[index] === undefined) {
                             p.innerText = " - ";
+                        } else {
+                            p.innerText = fourMoves[index].move.name.charAt(0).toUpperCase() + fourMoves[index].move.name.slice(1);
                         }
                     });
 
                 })
             }
-            , rej => console.error(rej))
+            , rej => {
+                console.error(rej);
+            });
+
     });
+}
 
-    //gets the data from the specified URL (speciesUrl)
-    async function data(url) {
-        const response = await fetch(url);
-        return await response.json();
-    }
-    //gets data about pokemon
-    async function pokemonData(searchValue) {
 
+//gets the data from the specified URL (speciesUrl)
+async function data(url) {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+//gets data about pokemon
+async function pokemonData(searchValue) {
+
+    try {
         const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + searchValue);
-        console.log(response)
         return await response.json();
+        //in case the user search for unknown pokemon
+    } catch (e) {
+        alert("Pokemon not found");
+        throw e;
     }
+}
 
-})
 
 
